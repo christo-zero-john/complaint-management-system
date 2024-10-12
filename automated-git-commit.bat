4b65bd08e296bce9bca99c66006285c3f60cb3ec
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-10-12 15:31:57 +0530

    Found modified or deleted file,  automated-git-commit.bat. Committing Via Global Bash Script:git-status-automated.bat

--- automated-git-commit.bat
+++ automated-git-commit.bat
@@ -1,13 +1,16 @@
 @REM Check for modified files and commit them
 
 @echo off
+
+git ls-files -m
 for /f "tokens=*" %%f in ('git ls-files -m') do (
     git add "%%f"
     git commit -m "Found modified or deleted file,  %%f. Committing Via Global Bash Script:git-status-automated.bat"
 )
 
 @REM check for new files and commit them
-@echo off
+
+git ls-files --others --exclude-standard
 for /f "tokens=*" %%f in ('git ls-files --others --exclude-standard') do (
     git add "%%f"
     git commit -m "Found new file,  %%f. Committing Via Global Bash Script:git-status-automated.bat"
