69d14faece6fec9e99c792d0ed14fbf77b163718
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-10-12 15:49:28 +0530

    Found modified or deleted file,  automated-git-commit.bat. Committing Via Global Bash Script:git-status-automated.bat

--- automated-git-commit.bat
+++ automated-git-commit.bat
@@ -3,27 +3,27 @@
 @echo off
 
 git ls-files -m
-echo -e "\n"
-echo -e "\n"
-echo -e "\n"
+echo "\n"
+echo "\n"
+echo "\n"
 for /f "tokens=*" %%f in ('git ls-files -m') do (
     git add "%%f"
     git commit -m "Found modified or deleted file,  %%f. Committing Via Global Bash Script:git-status-automated.bat"
 )
 
-echo -e "\n"
-echo -e "\n"
-echo -e "\n"
+echo "\n"
+echo "\n"
+echo "\n"
 
 
 @REM check for new files and commit them
-echo -e "\n"
-echo -e "\n"
-echo -e "\n"
+echo "\n"
+echo "\n"
+echo "\n"
 git ls-files --others --exclude-standard
-echo -e "\n"
-echo -e "\n"
-echo -e "\n"
+echo "\n"
+echo "\n"
+echo "\n"
 for /f "tokens=*" %%f in ('git ls-files --others --exclude-standard') do (
     git add "%%f"
     git commit -m "Found new file,  %%f. Committing Via Global Bash Script:git-status-automated.bat"
@@ -31,9 +31,9 @@ for /f "tokens=*" %%f in ('git ls-files --others --exclude-standard') do (
 
 @REM After commiting push to origin
 
-echo -e "\n"
+echo "\n"
 echo Starting push to Origin
-echo -e "\n"
-echo -e "\n"
+echo "\n"
+echo "\n"
 
 git push
