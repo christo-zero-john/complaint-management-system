5eea5316a2172e1ac1b829c320d9fdd4d640cfad
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-10-12 15:36:39 +0530

    Found modified or deleted file,  automated-git-commit.bat. Committing Via Global Bash Script:git-status-automated.bat

--- automated-git-commit.bat
+++ automated-git-commit.bat
@@ -17,5 +17,5 @@ for /f "tokens=*" %%f in ('git ls-files --others --exclude-standard') do (
 )
 
 @REM After commiting push to origin
-"\n\nStarting push to Origin\n\n"
+"Starting push to Origin"
 git push
