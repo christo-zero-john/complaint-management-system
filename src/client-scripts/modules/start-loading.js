8d18672520e3696016eb6463f7dde78bcfbc539e
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-10-12 15:30:25 +0530

    Found new file,  src/client-scripts/modules/start-loading.js. Committing Via Global Bash Script:git-status-automated.bat

---
+++ src/client-scripts/modules/start-loading.js
@@ -0,0 +1,13 @@
+function startLoading() {
+  console.log("started loading");
+  let loadingDiv;
+  if (!document.getElementById("loading-div")) {
+    loadingDiv = document.createElement("div");
+    loadingDiv.id = "loading-div";
+    loadingDiv.innerHTML = "Loading div";
+    document.body.appendChild(loadingDiv);
+  } else {
+    loadingDiv = document.getElementById("loading-div");
+  }
+  loadingDiv.classList.remove("hidden");
+}
