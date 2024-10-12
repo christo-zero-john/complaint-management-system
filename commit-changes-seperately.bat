56f329c21578e867062cc0aefbc670ebeb8ad972
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-10-12 15:30:16 +0530

    Found modified or deleted file,  commit-changes-seperately.bat. Committing Via Global Bash Script:git-status-automated.bat

--- commit-changes-seperately.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-for /f "tokens=*" %%f in ('git ls-files --others --exclude-standard') do (
-    git add "%%f"
-    git commit -m "Created, updated or deleted file,  %%f. Committing Via Global Bash Script."
-)
