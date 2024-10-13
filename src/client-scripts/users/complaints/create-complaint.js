59d1b9396b08d7a738059f7478ce31ec327007a2
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-10-13 16:37:51 +0530

    Found new file,  src/client-scripts/users/complaints/create-complaint.js. Committing Via Global Bash Script:git-status-automated.bat

---
+++ src/client-scripts/users/complaints/create-complaint.js
@@ -0,0 +1,174 @@
+console.log("Create complaint script loaded successfully");
+
+const server = "http://localhost/Complaint%20Management%20System/backend";
+
+fetchCategories();
+fetchDepartments();
+
+
+let fields = {
+  "complaint-title": document.getElementById("complaint-title"),
+  "complaint-description": document.getElementById("complaint-description"),
+  "complaint-category": document.getElementById("complaint-category"),
+  "complaint-department": document.getElementById("complaint-department"),
+  "complaint-visibility": document.getElementById("complaint-visibility"),
+};
+
+function createComplaint(event) {
+  // Prevent default form submission
+  event.preventDefault();
+
+  let complaintName = fields["complaint-title"].value;
+  let complaintDescription = fields["complaint-description"].value;
+  let complaintCategory = fields["complaint-category"].value;
+  let complaintDepartment = fields["complaint-department"].value;
+  let complaintVisibility = fields["complaint-visibility"].checked;
+
+  let missingFields = [];
+  if (!complaintName) {
+    missingFields.push("Title");
+    fields["complaint-title"].classList.add("empty-field");
+  }
+
+  if (!complaintDescription) {
+    missingFields.push("Description");
+    fields["complaint-description"].classList.add("empty-field");
+  }
+
+  if (!complaintCategory) {
+    missingFields.push("Category");
+    fields["complaint-category"].classList.add("empty-field");
+  }
+
+  if (!complaintDepartment) {
+    missingFields.push("Department");
+    fields["complaint-department"].classList.add("empty-field");
+  }
+
+  if (!complaintVisibility) {
+    missingFields.push("Visibility");
+    fields["complaint-visibility"].classList.add("empty-field");
+  }
+
+  // If some field values are missing notify that.
+  if (missingFields.length > 0) {
+    newNotification(`${missingFields.join(", ")} missing`);
+  }
+  // If all field values are received, send the data to the server.
+  else {
+    let data = {
+      name: complaintName,
+      description: complaintDescription,
+    };
+
+    let request = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+
+    console.log("Creating new complaint with data: ", request);
+    fetch(`${server}/api/complaints/new-complaint.php`, request)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        // If some error occurred, handle error
+        if (data.status === "error") {
+          data.errors.forEach((error) => {
+            // If error is complaint already exists
+            if (error.message === "Complaint Already Exists") {
+              newNotification("Complaint already exists.");
+            }
+            // If some unknown or unexpected error occurred.
+            else {
+              newNotification(
+                "Some error occurred unexpectedly." + error.details
+              );
+            }
+          });
+        }
+
+        // If server returned status as success, then display success message
+        else {
+          newNotification("Complaint created successfully.");
+          window.location.reload();
+        }
+      });
+  }
+}
+
+function fetchCategories() {
+  newNotification("Fetching all categories");
+
+  fetch(`${server}/api/categories/get-categories.php`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      if (data.status === "error") {
+        data.errors.forEach((error) => {
+          newNotification("An unexpected error occurred: " + error.details);
+        });
+      } else {
+        newNotification("Categories fetched successfully");
+        let categories = data.returned;
+
+        newNotification("Setting Category options");
+        fields["complaint-category"].innerHTML = "";
+
+        categories.forEach((category) => {
+          fields[
+            "complaint-category"
+          ].innerHTML += ` <option value="${category.id}">${category.name}</option>
+          `;
+        });
+      }
+    });
+}
+
+function fetchDepartments() {
+  newNotification("Fetching all departments");
+
+  fetch(`${server}/api/departments/get-departments.php`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      if (data.status === "error") {
+        data.errors.forEach((error) => {
+          newNotification("An unexpected error occurred: " + error.details);
+        });
+      } else {
+        newNotification("Departments fetched successfully");
+        let departments = data.returned;
+
+        newNotification("Setting Department options");
+        fields["complaint-department"].innerHTML = "";
+        departments.forEach((department) => {
+          fields[
+            "complaint-department"
+          ].innerHTML += ` <option value="${department.id}">${department.name}</option>
+          `;
+        });
+      }
+    });
+}
+
+function fetchDepartments() {}
+
+// Event Listners
+/**
+ * 1. When an input field is focused, remove empty-fields class from that field if it has the class empty-field.
+ * empty-field class is added to the element if the field value is null when create complaint is clicked.
+ * This event listner is used to remove the class when the field is clicked again once the field is focused again.
+ *
+ */
+let fieldElements = Object.values(fields);
+// console.log(fieldElements);
+fieldElements.forEach((element) => {
+  element.onfocus = (e) => {
+    if (e.target.classList.contains("empty-field")) {
+      e.target.classList.remove("empty-field");
+    }
+  };
+});
