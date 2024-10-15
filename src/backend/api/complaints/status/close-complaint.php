1211662b09fdbbe62c7d6ff5587f2a2244ce3687
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-10-15 20:57:37 +0530

    Found new file,  src/backend/api/complaints/status/close-complaint.php. Committing Via Global Bash Script:git-status-automated.bat

---
+++ src/backend/api/complaints/status/close-complaint.php
@@ -0,0 +1,51 @@
+<?php
+
+header("Access-Control-Allow-Origin: *"); // Allows requests from any origin
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); // Allow specific methods
+header("Access-Control-Allow-Headers: Content-Type"); // Allow specific headers
+
+require "../../modules/connectDatabase.php";
+require "../../modules/pushError.php";
+require "../../modules/parseRecievedData.php";
+require "../../modules/pushLog.php";
+
+
+/**
+ * @var $response Stores a predefined template of the response that is to be returned to the client side.
+ * @author `Christo John`.
+ 
+ * @property  $response["status"] - Stores the status of request. The status could be either of `success`, `failed`, `error`,  `warning`,  `info`,  `not_found`,   `invalid_request`,  `invalid_credentials`,  `invalid_token`,  `invalid_data`,   `invalid_parameters`,  `invalid_method`,  `invalid_url`, or `invalid_request_method`. 
+ * @property  $response["log"] - Stores an array of `logs`  that are to be returned to the client side. Each log is a `string` message that is suppossed to be printed in the  client side, or just for additional information.
+ * @property  $response["errors"] -  Stores an array of `errors` that are to be returned to the client side. Each error is an assosciative array with an error `code` and error `message`.
+ * @property  $response["returned"] - Store the actual data that is tobe send to the client side. It is the data after performing the `create-account` action. It contains  the `user_id` of the newly created user, along with a JWT token.
+ */
+$response = [
+    "status" => "success",
+    "logs" => [],
+    "errors" => [],
+    "returned" => [],
+];
+
+try {
+    pushLog("Initializing Re Open complaint");
+    $database = connectDB();
+    $data = parseRecievedData();
+
+    $id = $data["id"];
+    pushLog($data);
+
+    $query = "UPDATE complaints 
+    SET status = 0
+    WHERE id = $id";
+
+    pushLog(
+        "Re Opening complaint  with <b>ID:</b> $id."
+    );
+
+    mysqli_query($database, $query);
+    pushLog("Complaint Re Opened Successfully.");
+} catch (\Throwable $error) {
+    pushError($error);
+}
+
+echo json_encode($response);
