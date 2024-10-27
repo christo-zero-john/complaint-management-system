ca0339ecd84224c2dfbb438243c0573b8f1421e7
christo-zero-john <129399273+christo-zero-john@users.noreply.github.com>
2024-10-27 20:59:34 +0530

    Found new file,  src/client-scripts/modules/get-user-by-id.js. Committing Via Global Bash Script:git-status-automated.bat

---
+++ src/client-scripts/modules/get-user-by-id.js
@@ -0,0 +1,25 @@
+async function getUserById(UiD) {
+  if (UiD) {
+    console.log("Fetching user with ID: ", UiD);
+    if (UiD) {
+      let request = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id: UiD,
+        }),
+      };
+      return fetch(server + "/api/user/get-user-by-id.php", request)
+        .then((res) => res.json())
+        .then((data) => {
+          return data.returned;
+        });
+    } else {
+      throw new Error("User ID not specified");
+    }
+  } else {
+    return null;
+  }
+}
