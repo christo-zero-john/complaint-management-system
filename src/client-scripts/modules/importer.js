b3971cd30e9e414048f221c3dd183fd5b00319f3
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2024-12-04 09:26:18 +0530

    Found new file,  src/client-scripts/modules/importer.js. Committing Via Global Bash Script:git-status-automated.bat

---
+++ src/client-scripts/modules/importer.js
@@ -0,0 +1,68 @@
+import config from "./config.js";
+import { getUserById } from "./get-user-by-id.js";
+import { newNotification } from "./new-notification.js";
+import { startLoading } from "./start-loading.js";
+import { stopLoading } from "./stop-loading.js";
+import { fetchCategory } from "./get-category-by-id.js";
+import { fetchDepartment } from "./get-department-by-id.js";
+import { viewComplaints } from "../users/complaints/view-complaints.js";
+import { viewComplaintDetails } from "../users/complaints/view-complaint-details.js";
+import { editComplaint } from "../users/complaints/edit-complaint.js";
+import { deleteComplaint } from "../users/complaints/delete-complaint.js";
+import { closeComplaint } from "../users/complaints/close-complaint.js";
+import { reOpenComplaint } from "../users/complaints/reopen-complaint.js";
+import { myComplaints } from "../users/complaints/my-complaints.js";
+import { createComplaint } from "../users/complaints/create-complaint.js";
+import { fetchCategories } from "../users/complaints/create-complaint.js";
+import { fetchDepartments } from "../users/complaints/create-complaint.js";
+import { createCategory } from "../admin/manage-categories/create-category.js";
+import { editCategory } from "../admin/manage-categories/edit-category.js";
+import { viewCategories } from "../admin/manage-categories/view-categories.js";
+import { deleteCategory } from "../admin/manage-categories/delete-category.js";
+import { createDepartment } from "../admin/manage-departments/create-department.js";
+import {
+  editDepartment,
+  saveDepartment,
+} from "../admin/manage-departments/edit-department.js";
+import { deleteDepartment } from "../admin/manage-departments/delete-department.js";
+import { viewDepartments } from "../admin/manage-departments/view-departments.js";
+import { setFieldValues } from "./set-field-values.js";
+import { createRole } from "../admin/manage-roles/create-role.js";
+import { viewRoles } from "../admin/manage-roles/view-roles.js";
+import { editRole } from "../admin/manage-roles/edit-role.js";
+import { deleteRole } from "../admin/manage-roles/delete-role.js";
+
+console.clear();
+
+window.config = config;
+window.server = config.server;
+window.newNotification = newNotification;
+window.getUserById = getUserById;
+window.startLoading = startLoading;
+window.stopLoading = stopLoading;
+window.fetchCategory = fetchCategory;
+window.fetchDepartment = fetchDepartment;
+window.viewComplaints = viewComplaints;
+window.viewComplaintDetails = viewComplaintDetails;
+window.editComplaint = editComplaint;
+window.deleteComplaint = deleteComplaint;
+window.closeComplaint = closeComplaint;
+window.reOpenComplaint = reOpenComplaint;
+window.myComplaints = myComplaints;
+window.createComplaint = createComplaint;
+window.fetchCategories = fetchCategories;
+window.fetchDepartments = fetchDepartments;
+window.createCategory = createCategory;
+window.editCategory = editCategory;
+window.viewCategories = viewCategories;
+window.deleteCategory = deleteCategory;
+window.createDepartment = createDepartment;
+window.editDepartment = editDepartment;
+window.deleteDepartment = deleteDepartment;
+window.viewDepartments = viewDepartments;
+window.setFieldValues = setFieldValues;
+window.saveDepartment = saveDepartment;
+window.createRole = createRole;
+window.viewRoles = viewRoles;
+window.editRole = editRole;
+window.deleteRole = deleteRole;
