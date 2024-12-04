5ff77e8ec03c766a30fe18c0289c8f1f958f186d
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2024-12-04 09:26:14 +0530

    Found modified or deleted file,  src/client-scripts/modules/config.js. Committing Via Global Bash Script:git-status-automated.bat

--- src/client-scripts/modules/config.js
+++ src/client-scripts/modules/config.js
@@ -1,1 +1,5 @@
-const server = "http://localhost/Complaint%20Management%20System/backend";
+const config = {
+  server: "http://localhost/Complaint%20Management%20System/backend",
+};
+
+export default config;
