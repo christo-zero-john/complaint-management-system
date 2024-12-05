11703894cb86066b7671d2d4e46f9926c7272143
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2024-12-05 23:23:25 +0530

    Found modified or deleted file,  src/client-scripts/modules/set-field-values.js. Committing Via Global Bash Script:git-status-automated.bat

--- src/client-scripts/modules/set-field-values.js
+++
@@ -1,7 +0,0 @@
-function setFieldValues(event) {
-  console.log("Updating fieldsValues");
-  fieldsValues[event.target.id] = event.target.value;
-  console.log("fieldsValues changed: ", fieldsValues);
-}
-
-export { setFieldValues };
