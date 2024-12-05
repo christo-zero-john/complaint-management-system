378f2e9dde318e08525415ffa9f73e3fad6d7a96
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2024-12-05 23:23:49 +0530

    Found new file,  src/backend/api/complaints/new-complaint.php. Committing Via Global Bash Script:git-status-automated.bat

---
+++ src/backend/api/complaints/new-complaint.php
@@ -0,0 +1,68 @@
+<?php
+
+header("Access-Control-Allow-Origin: *"); // Allows requests from any origin
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); // Allow specific methods
+header("Access-Control-Allow-Headers: Content-Type"); // Allow specific headers
+
+require "../modules/connectDatabase.php";
+require "../modules/pushError.php";
+require "../modules/parseRecievedData.php";
+require "../modules/pushLog.php";
+
+
+/**
+ * @var $response Stores a predefined template of the response that is to be returned to the client side.
+ * @author `Christo John`.
+ 
+ * @property  $response["status"] - Stores the status of request. The status could be either of `success`, `failed`, `error`,  `warning`,  `info`,  `not_found`,   `invalid_request`,  `invalid_credentials`,  `invalid_token`,  `invalid_data`,   `invalid_parameters`,  `invalid_method`,  `invalid_url`, or `invalid_request_method`. 
+ * @property  $response["log"] - Stores an array of `logs`  that are to be returned to the client side. Each log is a `string` message that is suppossed to be printed in the  client side, or just for additional information.
+ * @property  $response["errors"] -  Stores an array of `errors` that are to be returned to the client side. Each error is an assosciative array with an error `code` and error `message`.
+ * @property  $response["returned"] - Store the actual data that is tobe send to the client side. It is the data after performing the `create-account` action. It contains  the `user_id` of the newly created user, along with a JWT token.
+
+
+
+
+ */
+$response = [
+    "status" => "success",
+    "logs" => [["recieved data" => parseRecievedData()]],
+    "errors" => [],
+    "returned" => [],
+];
+
+try {
+    pushLog("Initializing Create New Complaint");
+    $database = connectDB();
+    $data = parseRecievedData();
+
+    $title = $data["title"];
+    $description = $data["description"];
+    $category = $data["category"];
+    $department = $data["department"];
+    $is_private = $data["private"];
+    $created_by = $data["createdBy"];
+    $created_on = $data["created_on"];
+
+    $query = "INSERT INTO complaints(title, description, CiD, DiD, is_private, created_by_id, created_on) 
+    VALUES ('$title',  '$description', '$category', '$department', '$is_private', '$created_by', '$created_on')";
+
+    pushLog(
+        "<div>
+                <p>Creating New Complaint <b>$title</b> with 
+                <b>Description</b>:   <i>$description</i>
+                <b>Category ID</b>:   <i>$category</i>
+                <b>Departent ID</b>:   <i>$department</i>
+                <b>Visisbility</b>:   <i>$is_private</i>
+                <b>User ID</b>:   <i>$created_by</i>
+                <b>Create Date</b>:   <i>$created_on</i>
+                </p>
+                <p>And Empty parameters id, status, feedback and report</p>
+            </div>"
+    );
+    mysqli_query($database, $query);
+    pushLog("Complaint Created Successfully.");
+} catch (\Throwable $error) {
+    pushError($error);
+}
+
+echo json_encode($response);
