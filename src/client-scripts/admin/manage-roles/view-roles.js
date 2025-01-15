d9982d5437b8cc0f746e2cba7ad80836757cd514
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-01-15 23:01:56 +0530

    Found modified or deleted file,  src/client-scripts/admin/manage-roles/view-roles.js. Committing Via Global Bash Script:git-status-automated.bat

--- src/client-scripts/admin/manage-roles/view-roles.js
+++
@@ -1,9 +0,0 @@
-console.log("View roles script connected loaded successfully");
-
-function viewRoles() {
-  newNotification("Starting to display all roles");
-  let role = new Role();
-  role.viewRoles();
-}
-
-export { viewRoles };
