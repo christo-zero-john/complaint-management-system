4f3147beb2616852928d3ac01ee1df57c56f897e
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-01-15 23:01:55 +0530

    Found modified or deleted file,  src/client-scripts/admin/manage-categories/view-categories.js. Committing Via Global Bash Script:git-status-automated.bat

--- src/client-scripts/admin/manage-categories/view-categories.js
+++
@@ -1,9 +0,0 @@
-console.log("View categories script connected loaded successfully");
-
-function viewCategories() {
-  newNotification("Starting to display all categories");
-  let category = new Category();
-  category.viewCategories();
-}
-
-export { viewCategories };
