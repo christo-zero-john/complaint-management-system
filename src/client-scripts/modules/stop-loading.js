811d0dff8b1e92aab51b6395329c32677595b0b2
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-01-15 23:02:00 +0530

    Found modified or deleted file,  src/client-scripts/modules/stop-loading.js. Committing Via Global Bash Script:git-status-automated.bat

--- src/client-scripts/modules/stop-loading.js
+++
@@ -1,8 +0,0 @@
-function stopLoading() {
-  console.log("stopped loading");
-  let loadingDiv = document.getElementById("loading-div");
-
-  loadingDiv.classList.add("hidden");
-}
-
-export { stopLoading };
