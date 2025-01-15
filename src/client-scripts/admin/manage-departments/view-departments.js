27b3f8bc3bb726b7150f6ea1b8d0a5a42e5b1d4c
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-01-15 23:02:43 +0530

    Found new file,  src/client-scripts/admin/manage-departments/view-departments.js. Committing Via Global Bash Script:git-status-automated.bat

---
+++ src/client-scripts/admin/manage-departments/view-departments.js
@@ -0,0 +1,8 @@
+console.log("View Departments script connected loaded successfully");
+
+function viewDepartments() {
+  let department = new Department();
+  department.viewDepartments();
+}
+
+export { viewDepartments };
