fb42eac81f3897f3a5a8ef3b06071c769e1b93a8
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2025-01-15 23:01:56 +0530

    Found modified or deleted file,  src/client-scripts/admin/manage-departments/view-departments.js. Committing Via Global Bash Script:git-status-automated.bat

--- src/client-scripts/admin/manage-departments/view-departments.js
+++
@@ -1,8 +0,0 @@
-console.log("View Departments script connected loaded successfully");
-
-function viewDepartments() {
-  let department = new Department();
-  department.viewDepartments();
-}
-
-export { viewDepartments };
